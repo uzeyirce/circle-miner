db386e4dddedc199e0e05ede8f429c59d41bc729
uzeyirce <uzeyircelen@gmail.com>
2026-07-23 06:22:17 +0000

    fixed contract address

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,11 @@ module.exports = {
     },
   },
   networks: {
+    "arc-mainnet": {
+      url: process.env.ARC_MAINNET_RPC || "https://arc-mainnet.infura.io/v3/de58e8647ba54873a65e6b8d2d7bade7",
+      chainId: 5042,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
     "arc-testnet": {
       url: "https://rpc.testnet.arc.network",
       chainId: 5042002,
